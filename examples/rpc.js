b64495836e29cf86aa8c1bccac8d0f0ba57880a2
sven <sven@ledgit.be>
2019-04-23 15:03:10 +0200

    App.listen() changes #2

--- examples/rpc.js
+++ examples/rpc.js
@@ -1,6 +1,7 @@
 const brisk = require('..');
 const rpcRouter = require('../lib/middleware/rpc-router');
 const rpcLogger = require('../lib/middleware/rpc-logger');
+const rpcResponder = require('../lib/middleware/rpc-responder');
 
 const state = {
   db: {},
@@ -34,15 +35,17 @@ function requiresInit(req, res, next) {
 
 const app = brisk();
 
-app.address = "14yDSfMug1RxTRfAKC1mrfD1LqoBGSXUWc";
-app.from = 571999;
-
 // rpcRouter implements the following protocol:
 // txOutput 0: OP_RETURN <BITCOM_ID> <ROUTE> <JSON ENCODED ARGUMENTS ARRAY>
 // Fills req.route, req.args, req.caller
 app.use(rpcRouter);
-// Log them every time
+
+// Log rpc transactions every time
 app.use(rpcLogger);
+
+// Attach req.send if we're not syncing
+app.use(rpcResponder('L4PkQg1y39F1QfdmVJVZrb2DrEqxhSTzXtQ2gSJChmuoE5uRVqWo', (err, txid) => {console.log(err, txid)}));
+
 
 app.use('init', (req, res) => {
   if (!state.initialized) {
@@ -58,6 +61,7 @@ app.use('invite', requiresInit, mustBeParticipant, (req, res) => {
 app.use('set', requiresInit, mustBeParticipant, mustHaveArguments(2), (req, res) => {
   let [key, value] = req.args;
   state.db[key] = value;
+  res.send(['14yDSfMug1RxTRfAKC1mrfD1LqoBGSXUWc', 'confirm', JSON.stringify([req.tx.tx.h])]);
 });
 
 app.use('del', requiresInit, mustBeParticipant, mustHaveArguments(1), (req, res) => {
@@ -65,4 +69,10 @@ app.use('del', requiresInit, mustBeParticipant, mustHaveArguments(1), (req, res)
   delete state.db[key];
 });
 
-app.listen();
+app.use('confirm', (req, res) => {
+  if (req.caller === '14yDSfMug1RxTRfAKC1mrfD1LqoBGSXUWc') {
+    console.log('We confirmed transaction ' + req.args[0]);
+  }
+});
+
+app.listen('bitcom://14yDSfMug1RxTRfAKC1mrfD1LqoBGSXUWc', 570000);
