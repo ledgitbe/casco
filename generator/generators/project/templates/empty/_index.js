f301316d37034148c9f53b1214d81ef6ec98b6cf
sven <sven@ledgit.be>
2019-05-04 18:43:32 +0200

    Better empty project

--- generator/generators/project/templates/empty/_index.js
+++ generator/generators/project/templates/empty/_index.js
@@ -1,20 +1,23 @@
 const ledgit = require('ledgit');
+const router = require('ledgit/lib/middleware/router-s2json');
+const logger = require('ledgit/lib/middleware/logger');
+const validator = require('ledgit/lib/middleware/validator');
 
 require('dotenv').config();
 
 const app = ledgit();
 
-/*
- * Define your application handlers here
+app.use(router);
+app.use(logger);
+
+/* This is an example handler 
+ * 
+ * Fund your client and then run the client like this:
+ * `node client.js set hello world
  *
- * For example:
- *
- * app.use((req, res) => {
- *  console.log("New tx!");
- *  console.log(req.tx);
- *  })
- *
- * Check out the other examples and the documentation.
+ * app.use('set', validator({ key: "string", value: "any"}), (req, res) => {
+ *   console.log(`${req.caller} is setting ${key} to ${value}`);
+ * });
  *
  */
 
