dafe91efc9a58d8a839b250db31e701ca8c7d8aa
sven <sven@ledgit.be>
2019-07-04 14:37:23 +0200

    Rename to casco + Update npm dependencies

--- packages/casco/lib/index.js
+++ packages/casco/lib/index.js
@@ -1,9 +1,9 @@
 /*
  * Copyright (C) 2019 Ledgit bvba
  *
- * This file is part of Ledgit
+ * This file is part of Casco
  *
- * Ledgit is free software: you can redistribute it and/or modify
+ * Casco is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
